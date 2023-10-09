744a209c83dd57e7addbf1e508142608883bd2e2
mariagladij <145344925+mariagladij@users.noreply.github.com>
2023-10-09 09:42:50 +0200

    Add files via upload

---
+++ lesson7/src/main/java/ua/hillel/qaauto/Person.java
@@ -0,0 +1,12 @@
+package ua.hillel.qaauto;
+
+public class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
